922d0fe88e1d12b3003fa3ab48baf0a3599a427b
slish01 <roninbarker@gmail.com>
2026-01-06 08:55:21 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,13 +17,13 @@ if st.button("Strong"):
     st.session_state.mode = "strong"
 
 if st.session_state.mode == "weak":
-    words = ["copper", "goon", "monkey", "blueberry", "yogurt", "green", "leopard", "pizza", "lemon", "iron",
+    words = ["copper", "monkey", "blueberry", "yogurt", "green", "leopard", "pizza", "lemon", "iron",
              "lime", "brown", "deck", "clock", "yellow", "blue", "door", "apple", "banana", "noodle", "blue", "television",
              "vehicle", "germany", "canada", "america", "octopus", "beer", "emerald", "koala", "kangaroo", "pill", "window",
              "plane", "backflip", "vitamin", "figure", "dance", "music", "piano", "bird", "ostrich", "giraffe"]
     numbers = [str(i) for i in range(1000)]
     password = random.choice(words) + random.choice(numbers)
-    st.error("Maybe consider the other option there bud")
+    st.error("Maybe consider the other option there")
     st.success(password)
 
 if st.session_state.mode == "strong":
@@ -42,11 +42,12 @@ if st.session_state.mode == "strong":
         chars += string.punctuation
 
     if chars == "":
-        st.error("Are you slow?")
+        st.error("You can't really make a password without any characters")
     else:
         password = "".join(random.choice(chars) for _ in range(length))
         st.write("**Here is your password:**")
         st.success(password)
         st.write("Enjoy your password, keep it in a safe place where you will remember it. :)")
-        st.write("**If password does not meet your site's requirements try adding characters to the password to meet the requirements :)**")
+        st.write("**If password does not meet your site's requirements try adding characters to the password to meet the requirements**")
         st.write("<span style='color:red;'>**NEVER SHARE YOUR PASSWORD WITH OTHERS!**</span>", unsafe_allow_html=True)
+
